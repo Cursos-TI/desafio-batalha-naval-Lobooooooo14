ebc3f1ad514445b70020caf48b1ffcadfa128f1e
Lobo <88998991+Lobooooooo14@users.noreply.github.com>
2025-02-06 20:28:10 -0300

    aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -2,18 +2,64 @@
 
 int main()
 {
-    int matriz[8][8] = {
-        {1, 2, 3, 4, 5, 6, 7, 8},
-        {9, 10, 11, 12, 13, 14, 15, 16},
-        {17, 18, 19, 20, 21, 22, 23, 24},
-        {25, 26, 27, 28, 29, 30, 31, 32},
-        {33, 34, 35, 36, 37, 38, 39, 40},
-        {41, 42, 43, 44, 45, 46, 47, 48},
-        {49, 50, 51, 52, 53, 54, 55, 56},
-        {57, 58, 59, 60, 61, 62, 63, 64},
-    };
+    int matrix[10][10] = {};
+    int startLine, startCol; // for diagonal ships
 
-    printf("Parte do navio posicionado na casa %d", matriz[3][3]);
+    // fill matrix
+    for (int l = 0; l < 10; l++)
+    {
+        for (int c = 0; c < 10; c++)
+        {
+            matrix[l][c] = 0;
+        }
+    }
+
+    // create vertical and horizontal ships
+    for (int l = 0; l < 10; l++)
+    {
+        for (int c = 0; c < 10; c++)
+        {
+            // vertical
+            if (c == 2 && l > 2 && l < 7)
+            {
+                matrix[l][c] = 3;
+            }
+
+            // horizontal
+            if (l == 8 && c > 3 && c < 8)
+            {
+                matrix[l][c] = 3;
+            }
+        }
+    }
+
+    // create diagonal up right ship
+    startLine = 5;
+    startCol = 5;
+
+    for (int i = 0; i < 4; i++)
+    {
+        matrix[startLine - i][startCol + i] = 3;
+    }
+
+    // create diagonal down left ship
+    startLine = 0;
+    startCol = 1;
+
+    for (int i = 0; i < 4; i++)
+    {
+        matrix[startLine + i][startCol + i] = 3;
+    }
+
+    // show matrix
+    for (int l = 0; l < 10; l++)
+    {
+        for (int c = 0; c < 10; c++)
+        {
+            printf("%d ", matrix[l][c]);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
